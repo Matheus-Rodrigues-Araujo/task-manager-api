a378c1c411040dcafefe08520e429296f0d3622e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-06 19:43:26 -0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://taskgoalapp.vercel.app',  // Defina o domínio do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',  // Permite headers customizados como Authorization
   });
   await app
     .listen(8000)
