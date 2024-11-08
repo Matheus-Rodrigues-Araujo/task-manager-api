c3340675819fa8b6c694b4e5a738c3b8ddad430f
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-08 18:37:54 -0300

    refactor: enable CORS for FRONTEND_URL

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const frontendUrl = process.env.FRONTEND_URL?.endsWith('/')
+    ? process.env.FRONTEND_URL.slice(0, -1)
+    : process.env.FRONTEND_URL;
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: frontendUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
